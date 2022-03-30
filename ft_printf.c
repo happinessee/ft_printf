f0d230c310c4b3b54fec93951690326400c22ab2
happinessee <gygur1206@gmail.com>
2022-03-30 17:32:42 +0900

    printf designing....

--- ft_printf.c
+++ ft_printf.c
@@ -6,16 +6,33 @@
 /*   By: hyojeong <hyojeong@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/29 17:58:39 by hyojeong          #+#    #+#             */
-/*   Updated: 2022/03/29 18:00:23 by hyojeong         ###   ########.fr       */
+/*   Updated: 2022/03/30 17:32:15 by hyojeong         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 
 #include <stdlib.h>
+#include <stdarg.h>
 
-
-int	ft_printf(const char str)
+int branch(char *str, ...)
 {
 
+	return (1);
+}
+
+int	ft_printf(const char *str, ...)
+{
+	while (1)
+	{
+		if (str && !(str == '%'))
+		{
+			write(1, str, 1);
+		}
+		else
+		{
+			
+		}
+		str++;
+	}
 }
